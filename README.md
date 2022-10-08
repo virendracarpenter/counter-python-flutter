5ace2e85dfe562ff67e5808bfda3383984d0554b
Virendra Carpenter <viren.28opensky@gmail.com>
2022-10-08 17:52:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# counter-python-flutter
---
+++ counter.py
@@ -0,0 +1,29 @@
+import flet
+from flet import Page, TextField, Row, IconButton, icons
+
+def main(page: Page):
+    page.title = "Counter App"
+    page.vertical_alignment = "center"
+
+    txt_field = TextField(value="0", width=100, text_align="right")
+
+    def minux_clicked(e):
+        txt_field.value = int(txt_field.value) -1
+        page.update()
+
+    def plus_clicked(e):
+        txt_field.value = int(txt_field.value) +1
+        page.update()
+
+    page.add(
+        Row(
+            [
+                IconButton(icons.REMOVE, on_click=minux_clicked),
+                txt_field,
+                IconButton(icons.ADD, on_click=plus_clicked),
+            ], 
+            alignment="center"
+        )
+    )
+
+flet.app(target=main, view=flet.WEB_BROWSER)
